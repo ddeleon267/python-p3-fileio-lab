30ebe3164295f4d3135340118483895e1e7b123a
ddeleon267 <dominique.a.deleon@gmail.com>
2022-08-17 17:15:20 -0400

    Done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,25 @@
+import ipdb
+
 def write_file(file_name, file_content):
-    pass
+    # ipdb.set_trace()
+    with open(file_name + ".txt", mode='w', encoding='utf-8') as log_file: log_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(file_name + ".txt", mode='a', encoding='utf-8') as log_file: log_file.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    with open(file_name + '.txt') as text_file:
+        return text_file.read()
+
+
+# Lets practice! In the file file_io.py write a function called write_file that takes in 
+# the arguments file_name and file_content. The file_name can be a combined file path/name.
+#  This function should use file_name and file_content to write a txt file.
+
+# Write a append_to_file function that takes in the same parameters and appends to the .txt
+#  file.
+
+# Write a read_file function that takes in a file name and returns the content of the .txt 
+# file.
+
